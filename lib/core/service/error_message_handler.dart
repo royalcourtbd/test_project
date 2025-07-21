224c1bb0870632520ca7f4c28a565ee75442f19e
MD ABU SAYED <62033575+royalcourtbd@users.noreply.github.com>
2025-07-21 15:39:09 +0600

    Update error_message_handler.dart

--- lib/core/service/error_message_handler.dart
+++ lib/core/service/error_message_handler.dart
@@ -1,3 +1,15 @@
 abstract class ErrorMessageHandler {
   String generateErrorMessage(Object error);
 }
+
+
+class ErrorMessageHandlerImpl implements ErrorMessageHandler {
+  @override
+  String generateErrorMessage(Object error) {
+    if (error is FormatException) {
+      return 'Data format is not correct';
+    }
+
+    return error.toString();
+  }
+}
