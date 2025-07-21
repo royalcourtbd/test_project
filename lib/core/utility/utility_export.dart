1050a7156e44920f3b14faa9fa43cebbb4fdd1f6
MD ABU SAYED <62033575+royalcourtbd@users.noreply.github.com>
2025-07-21 14:25:19 +0600

    Create utility_export.dart

---
+++ lib/core/utility/utility_export.dart
@@ -0,0 +1,8 @@
+export 'trial_utility.dart';
+export 'utility.dart';
+export 'dart:developer';
+export 'dart:io';
+export 'package:intl/intl.dart';
+export 'package:package_info_plus/package_info_plus.dart';
+export 'package:path_provider/path_provider.dart';
+export 'package:initial_project/core/static/constants.dart';
